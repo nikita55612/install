bf7725d4a22c5e30caa57a415c46ee954ba93419
Nikita <groz.yt56@gmail.com>
2025-10-27 08:51:08 +0300

    install proxy

--- proxyserv/install.sh
+++ proxyserv/install.sh
@@ -8,6 +8,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 SetBBR() {
+    touch /etc/sysctl.conf
     grep -q "^net.core.default_qdisc=fq" /etc/sysctl.conf || echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
     grep -q "^net.ipv4.tcp_congestion_control=bbr" /etc/sysctl.conf || echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
     /usr/sbin/sysctl -p >/dev/null
@@ -70,7 +71,7 @@ if [[ -z "$newusername" ]]; then
 fi
 
 adduser "$newusername"
-usermod -aG "$newusername"
+usermod -aG sudo "$newusername"
 
 mkdir -p /home/"$newusername"/.ssh
 
